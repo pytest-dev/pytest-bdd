e078457f1c1d2501f6148606c6857aa06804ac54
Oleg Podsadny <oleg.podsadny@gmail.com>
2013-06-16 15:52:20 +0200

    fixing the setup.py for the pypi

--- setup.py
+++ setup.py
@@ -18,19 +18,35 @@ class PyTest(TestCommand):
         errno = pytest.main(self.test_args)
         sys.exit(errno)
 
+long_description = open('README.md').read()
 
 setup(
     name='pytest-bdd',
     description='BDD for pytest',
+    long_description=long_description,
     author='Oleg Pidsadnyi',
+    license='MIT license',
     author_email='oleg.podsadny@gmail.com',
-    version='0.4.1',
+    version='0.4.2',
+    classifiers=[
+        'Development Status :: 6 - Mature',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: POSIX',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Software Development :: Testing',
+        'Topic :: Software Development :: Libraries',
+        'Topic :: Utilities',
+        'Programming Language :: Python :: 2',
+        'Programming Language :: Python :: 3'
+    ] + [('Programming Language :: Python :: %s' % x) for x in '2.6 2.7 3.0 3.1 3.2 3.3'.split()],
     cmdclass={'test': PyTest},
     install_requires=[
         'pytest',
     ],
     # the following makes a plugin available to py.test
-    entry_points = {
+    entry_points={
         'pytest11': [
             'pytest-bdd = pytest_bdd.plugin',
         ]
