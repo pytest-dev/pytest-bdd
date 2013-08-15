d8ec8b4479e277d9b5f7ed53aa6636ff364e0ddf
Anatoly Bubenkov <bubenkoff@gmail.com>
2013-08-15 11:59:18 +0200

    python2.5 support

--- tests/feature/test_feature_base_dir.py
+++ tests/feature/test_feature_base_dir.py
@@ -21,6 +21,6 @@ def pytestbdd_feature_base_dir():
 def test_feature_path(request, scenario_name):
     """Test feature base dir."""
     sc = scenario('steps.feature', scenario_name)
-    pytest.raises(IOError, sc, request)
+    exc = pytest.raises(IOError, sc, request)
 
     assert os.path.join('/does/not/exist/', 'steps.feature') in str(exc.value)
