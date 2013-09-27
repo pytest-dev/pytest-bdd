5492b7332290799305b96a0b839f59918ead711a
Andrey Makhnach <andrey.makhnach@gmail.com>
2013-09-27 11:09:33 +0300

    Added tests to failing steps with arguments.

--- tests/feature/test_wrong.py
+++ tests/feature/test_wrong.py
@@ -1,4 +1,6 @@
 """Test wrong feature syntax."""
+import functools
+import re
 
 import pytest
 
@@ -69,3 +71,21 @@ def test_verbose_output(request):
 
     assert line_number == 4
     assert line == 'When I do it again'
+
+
+sc = functools.partial(scenario, 'failing_arguments.feature')
+test_argument_in_when_step_1 = sc('Argument in when, step 1')
+test_argument_in_when_step_2 = sc('Argument in when, step 2')
+
+
+@pytest.fixture
+@when(re.compile('I get argument (?P<arg>\d+)'))
+def get_argument(arg):
+    """Getting argument."""
+    return dict(arg=arg)
+
+
+@then(re.compile('My argument should be (?P<arg>\d+)'))
+def assert_that_my_argument_is_arg(get_argument, arg):
+    """Assert that arg from when equals arg."""
+    assert get_argument['arg'] == arg
