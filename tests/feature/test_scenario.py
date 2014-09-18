771f263ec6fc90334e16ff6aaf7195b6ce17cbda
Anatoly Bubenkov <bubenkoff@gmail.com>
2014-09-18 18:32:50 +0000

    Better reporting of a not found scenario

--- tests/feature/test_scenario.py
+++ tests/feature/test_scenario.py
@@ -8,11 +8,13 @@ from pytest_bdd import exceptions
 def test_scenario_not_found(request):
     """Test the situation when scenario is not found."""
 
-    with pytest.raises(exceptions.ScenarioNotFound):
+    with pytest.raises(exceptions.ScenarioNotFound) as exc_info:
         scenario(
             'not_found.feature',
             'NOT FOUND'
         )
+    assert exc_info.value.args[0].startswith('Scenario "NOT FOUND" in feature "[Empty]" in {feature_path}'.format(
+        feature_path=request.fspath.join('..', 'not_found.feature')))
 
 
 def test_scenario_comments(request):
