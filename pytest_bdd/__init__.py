e313294c779dcf8a3ef495cffeb04ae38cabc832
Anatoly Bubenkov <bubenkoff@gmail.com>
2015-06-21 00:38:02 +0200

    Feature example table. closes #131

--- pytest_bdd/__init__.py
+++ pytest_bdd/__init__.py
@@ -3,6 +3,6 @@
 from pytest_bdd.steps import given, when, then
 from pytest_bdd.scenario import scenario, scenarios
 
-__version__ = '2.12.2'
+__version__ = '2.13.0'
 
 __all__ = [given.__name__, when.__name__, then.__name__, scenario.__name__, scenarios.__name__]
