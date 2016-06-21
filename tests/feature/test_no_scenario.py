887dac1a3d78a1a1f906f2b9086f2faee5e95762
Oleg Pidsadnyi <oleg.pidsadnyi@gmail.com>
2016-06-21 12:49:01 +0200

    Better error explanation for the steps defined outside of scenarios

---
+++ tests/feature/test_no_scenario.py
@@ -0,0 +1,24 @@
+"""Test no scenarios defined in the feature file."""
+
+import textwrap
+
+
+def test_no_scenarios(testdir):
+    """Test no scenarios defined in the feature file."""
+    features = testdir.mkdir('features')
+    features.join('test.feature').write_text(textwrap.dedent(u"""
+        Given foo
+        When bar
+        Then baz
+    """), 'utf-8', ensure=True)
+    testdir.makepyfile("""
+        from pytest_bdd import scenarios
+
+        scenarios('features')
+    """)
+    result = testdir.runpytest('-v', '-s')
+    result.stdout.fnmatch_lines(
+        [
+            '*pytest_bdd.exceptions.FeatureError: Step definition outside of a Scenario or a Background.*',
+        ],
+    )
