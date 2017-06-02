aae9dd164471f99c65e002e7457deccf6a17c796
Franco Reyes <freyes1988@gmail.com>
2017-06-01 22:44:51 -0500

    WIP of the fixtures man

--- tests/feature/test_datatable_fixture_collision.py
+++ tests/feature/test_datatable_fixture_collision.py
@@ -9,10 +9,13 @@ from pytest_bdd import (
 
 import pytest
 
+# Existing fixture
+
 @pytest.fixture
 def datatable():
     return [['header'],['test body']]
 
+# Scenarios
 @scenario('test_datatable_fixture_collision.feature', 'Ensure that there is an existing datatable fixture')
 def test_ensure_existing_fixture():
     """Ensure existing fixture is used."""
@@ -21,18 +24,25 @@ def test_ensure_existing_fixture():
 def test_datatable_does_not_conflict_with_existing_fixture():
     """Ensure that there is no collision between existing fixture and datatable"""
 
+# Steps
+
 @given('There is an existing fixture named datatable')
 def there_is_an_existing_datatable_fixture(datatable):
     return datatable
 
-@given('I have the following cars')
+@given('I have the following cars:')
 def i_have_the_following_cars(datatable):
     return datatable
 
+@then("datatable contents match existing fixture")
+def check_that_contents_match(there_is_an_existing_datatable_fixture, datatable):
+    assert there_is_an_existing_datatable_fixture == datatable
 
-@then(parsers.re('datatable contents (?P<negation>(don\'t |do not )?)match existing fixture'))
-def check_if_contents_match_existing(negation, there_is_an_existing_datatable_fixture, datatable):
-    if negation:
-        assert there_is_an_existing_datatable_fixture != datatable
-    else:
-        assert there_is_an_existing_datatable_fixture == datatable
+@then("datatable contents don't match existing fixture")
+def check_that_contents_dont_match(i_have_the_following_cars, datatable):
+    assert i_have_the_following_cars != datatable
+
+@then('I should see the following models:')
+def i_should_see_following_models(i_have_the_following_cars, datatable):
+    column =  i_have_the_following_cars[0][1]
+    assert column == 'model'
