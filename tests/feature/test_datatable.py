9a0ffa54d0ea5e5b8a423638a3bb8a61446d4127
Franco Reyes <freyes1988@gmail.com>
2017-07-21 23:33:41 -0500

    Comply with the pep8 stuff

--- tests/feature/test_datatable.py
+++ tests/feature/test_datatable.py
@@ -11,9 +11,11 @@ from pytest_bdd import (
 def test_i_use_datatables():
     """I use datatables."""
 
+
 @scenario('test_datatable.feature', 'I use datatables for emails')
 def test_i_use_datatables_for_emails():
     """I use datatables for emails."""
+
 
 @given('the following users exist:')
 def the_following_users_exist(datatable):
@@ -25,6 +27,7 @@ def the_following_users_exist(datatable):
 def i_should_see(datatable, the_following_users_exist):
     names = [row[0] for row in the_following_users_exist]
     assert names == sum(datatable[1], [])
+
 
 @then('I should see the following emails:')
 def i_should_see(datatable, the_following_users_exist):
