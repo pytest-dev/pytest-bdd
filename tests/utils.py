432e0e05c7af6b925f6450d2800a1d45506d9391
Milosz Sliwinski <sliwinski.milosz@gmail.com>
2018-12-19 11:02:01 +0100

    Linter fixes

--- tests/utils.py
+++ tests/utils.py
@@ -5,9 +5,11 @@ def get_test_filepath(filepath):
     curr_file_dirpath = os.path.dirname(os.path.realpath(__file__))
     return os.path.join(curr_file_dirpath, filepath)
 
+
 def get_filename_without_ext(path):
     filename = os.path.basename(path)
     return os.path.splitext(filename)[0]
+
 
 def prepare_feature_and_py_files(testdir, feature_file, py_file):
     feature_filepath = get_test_filepath(feature_file)
