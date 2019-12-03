80c0416767ed1009edfbeb899bf094748dff7f94
Ryan Casperson <casperson.ryan@gmail.com>
2019-12-03 15:24:33 -0500

    Update tests to utilize pytester plugin

--- tests/feature/test_description.py
+++ tests/feature/test_description.py
@@ -2,6 +2,30 @@
 from pytest_bdd import scenario
 
 
+test_file_contents = """
+import pytest
+from pytest_bdd import given, scenario
+
+def test_descriptions(request):
+    @scenario(
+        "descriptions.feature",
+        "Description",
+    )
+    def test():
+        pass
+
+    test(request)
+
+@given("I have a bar")
+def nothing():
+    pass
+
+"""
+
+with open("./tests/feature/description.feature") as f:
+    feature_file_contents = f.read()
+
+
 def test_description(request):
     """Test description for the feature."""
 
@@ -18,9 +42,20 @@ Because it will be cool
 
 Some description goes here."""
     )
-    assert test.__scenario__.description == """Also, the scenario can have a description.
+    assert (
+        test.__scenario__.description
+        == """Also, the scenario can have a description.
 
 It goes here between the scenario name
 and the first step."""
+    )
 
     test(request)
+
+
+def test_scenarios_are_created_when_they_have_scenario_descriptions(testdir):
+    testdir.makepyfile(test_descriptions=test_file_contents)
+    testdir.makefile(".feature", descriptions=feature_file_contents)
+
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
--- tests/feature/test_scenario_descriptions.py
+++
@@ -1,4 +0,0 @@
-from pytest_bdd import scenarios
-
-# See feature file for reasons for executing (skipping) these tests
-scenarios("scenario_descriptions.feature")
