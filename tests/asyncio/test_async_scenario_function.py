e4020aeb22856eeef64a2fbe46bcce42e31f0281
Amadeusz Hercog <xaaq333@gmail.com>
2020-02-03 20:25:19 +0100

    add tests for checking if async scenario is launched

---
+++ tests/asyncio/test_async_scenario_function.py
@@ -0,0 +1,54 @@
+import textwrap
+
+import pytest
+
+
+@pytest.fixture
+def feature_file(testdir):
+    testdir.makefile(
+        ".feature",
+        test=textwrap.dedent(
+            """
+            Feature: Async scenario function is being launched
+
+                Scenario: Launching scenario function
+            """
+        ),
+    )
+
+
+def test_scenario_function_marked_with_async_passes(feature_file, testdir):
+    testdir.makepyfile(
+        textwrap.dedent(
+            """
+            import pytest
+            from pytest_bdd import scenario
+
+            @pytest.mark.asyncio
+            @scenario('test.feature', 'Launching scenario function')
+            async def test_launching_scenario_function():
+                pass
+            """
+        )
+    )
+
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_scenario_function_not_marked_with_async_fails(feature_file, testdir):
+    testdir.makepyfile(
+        textwrap.dedent(
+            """
+            import pytest
+            from pytest_bdd import scenario
+
+            @scenario('test.feature', 'Launching scenario function')
+            async def test_launching_scenario_function():
+                pass
+            """
+        )
+    )
+
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
