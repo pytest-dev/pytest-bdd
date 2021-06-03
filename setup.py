34f68dc758ad36f429d4c1186da1acc542792e03
Thomas Grainger <tagrain@gmail.com>
2021-06-03 18:32:57 +0100

    support async scenarios

--- setup.py
+++ setup.py
@@ -1,3 +1,12 @@
+import unasync
 from setuptools import setup
 
-setup()
+setup(
+    cmdclass={
+        "build_py": unasync.cmdclass_build_py(
+            rules=[
+                unasync.Rule("/pytest_bdd/_async/", "/pytest_bdd/_sync/"),
+            ]
+        )
+    }
+)
