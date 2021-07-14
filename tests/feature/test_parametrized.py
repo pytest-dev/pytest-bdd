d1ff34a69912eeec5221fc752e9b5d9d2c93f591
Konstantin Goloveshko <konstantin.goloveshko@collabio.team>
2021-07-14 13:32:45 +0300

    Allows use <params> in parsers defined steps
    So parsers could parse values defined in Examples

--- tests/feature/test_parametrized.py
+++ tests/feature/test_parametrized.py
@@ -59,3 +59,54 @@ def test_parametrized(testdir):
     )
     result = testdir.runpytest()
     result.assert_outcomes(passed=3)
+
+
+def test_parametrized_with_parsers(testdir):
+    """Test parametrized scenario."""
+    testdir.makefile(
+        ".feature",
+        parametrized=textwrap.dedent(
+            """\
+            Feature: Parametrized scenario
+                Scenario: Parametrized given, when, thens with parsers invocation
+                    Given there are <start> gherkins
+                    When I eat <eat> gherkins
+                    Then I should have <left> gherkins
+            """
+        ),
+    )
+
+    testdir.makepyfile(
+        textwrap.dedent(
+            """\
+        import pytest
+        from pytest_bdd import given, when, then, scenario
+        from pytest_bdd.parsers import re, parse 
+
+        @pytest.mark.parametrize(["start", "eat", "left"], [(12, 5, 7)])
+        @scenario("parametrized.feature", "Parametrized given, when, thens with parsers invocation")
+        def test_parametrized(request, start, eat, left):
+            pass
+
+        
+        @given(re("there are <start> (?P<vegetables>\\\\w+)"), target_fixture="start_vegetables")
+        def start_vegetables(start, vegetables):
+            return dict(start=start)
+
+
+        @when("I eat <eat> gherkins")
+        def eat_cucumbers(start_vegetables, start, eat):
+            start_vegetables["eat"] = eat
+
+
+        @then(re("I should have (?P<left>\\\\d+) (?P<vegetables>\\\\w+)"), converters=dict(left=int))
+        def should_have_left_vegetables(start_vegetables, start, eat, left, vegetables):
+            assert start - eat == left
+            assert start_vegetables["start"] == start
+            assert start_vegetables["eat"] == eat
+
+        """
+        )
+    )
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
