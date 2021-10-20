12dbf66f4d881c1db428718ad28ae3af33eb4aec
Alessio Bogon <youtux@gmail.com>
2021-10-20 16:07:04 +0200

    Disable type collection

--- tests/conftest.py
+++ tests/conftest.py
@@ -25,27 +25,27 @@ def pytest_generate_tests(metafunc):
 
 
 # TODO: Remove these before merge
-def pytest_collection_finish(session):
-    """Handle the pytest collection finish hook: configure pyannotate.
-    Explicitly delay importing `collect_types` until all tests have
-    been collected.  This gives gevent a chance to monkey patch the
-    world before importing pyannotate.
-    """
-    from pyannotate_runtime import collect_types
-
-    collect_types.init_types_collection()
-
-
-@pytest.fixture(autouse=True)
-def collect_types_fixture() -> Iterator:
-    from pyannotate_runtime import collect_types
-
-    collect_types.start()
-    yield
-    collect_types.stop()
-
-
-def pytest_sessionfinish(session, exitstatus):
-    from pyannotate_runtime import collect_types
-
-    collect_types.dump_stats("type_info.json")
+# def pytest_collection_finish(session):
+#     """Handle the pytest collection finish hook: configure pyannotate.
+#     Explicitly delay importing `collect_types` until all tests have
+#     been collected.  This gives gevent a chance to monkey patch the
+#     world before importing pyannotate.
+#     """
+#     from pyannotate_runtime import collect_types
+#
+#     collect_types.init_types_collection()
+#
+#
+# @pytest.fixture(autouse=True)
+# def collect_types_fixture() -> Iterator:
+#     from pyannotate_runtime import collect_types
+#
+#     collect_types.start()
+#     yield
+#     collect_types.stop()
+#
+#
+# def pytest_sessionfinish(session, exitstatus):
+#     from pyannotate_runtime import collect_types
+#
+#     collect_types.dump_stats("type_info.json")
