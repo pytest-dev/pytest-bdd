c4b8da78d1e124068a561ad5cd699f83ec3f10b5
Kostiantyn Goloveshko <KGoloveshko@luxoft.com>
2022-01-03 23:33:49 +0200

    Switch default parser to cfparse

--- pytest_bdd/parsers.py
+++ pytest_bdd/parsers.py
@@ -3,44 +3,61 @@
 
 import re as base_re
 import sys
-from functools import partial
+from abc import ABCMeta, abstractmethod
+from typing import Any, Dict, Union
 
 import parse as base_parse
 from parse_type import cfparse as base_cfparse
 
+if sys.version_info >= (3, 8):
+    from typing import Protocol, runtime_checkable
+else:
+    try:
+        from typing_extensions import Protocol, runtime_checkable
+    except ImportError:
+        Protocol, runtime_checkable = object, lambda cls: cls
+
 _Re_Pattern = base_re.Pattern if sys.version_info >= (3, 7) else type(base_re.compile(""))
 
 
-class StepParser:
+@runtime_checkable
+class StepParserProtocol(Protocol):
+    name: Any
+
+    def parse_arguments(self, name) -> Dict[str, Any]:
+        ...
+
+    def is_matching(self, name) -> Union[bool, Any]:
+        ...
+
+
+class StepParser(StepParserProtocol, metaclass=ABCMeta):
     """Parser of the individual step."""
 
     def __init__(self, name):
         self.name = name
 
+    @abstractmethod
     def parse_arguments(self, name):
         """Get step arguments from the given step name.
 
         :return: `dict` of step arguments
         """
-        raise NotImplementedError()  # pragma: no cover
+        raise NotImplemented  # pragma: no cover
 
+    @abstractmethod
     def is_matching(self, name):
         """Match given name with the step name."""
-        raise NotImplementedError()  # pragma: no cover
+        raise NotImplemented  # pragma: no cover
 
 
 class _CommonRe(StepParser):
     regex: _Re_Pattern
 
     def parse_arguments(self, name):
-        """Get step arguments.
-
-        :return: `dict` of step arguments
-        """
         return self.regex.match(name).groupdict()
 
     def is_matching(self, name):
-        """Match given name with the step name."""
         return bool(self.regex.match(name))
 
 
@@ -64,14 +81,9 @@ class _CommonParse(StepParser):
     parser: base_parse.Parser
 
     def parse_arguments(self, name):
-        """Get step arguments.
-
-        :return: `dict` of step arguments
-        """
         return self.parser.parse(name).named
 
     def is_matching(self, name):
-        """Match given name with the step name."""
         try:
             return bool(self.parser.parse(name))
         except ValueError:
@@ -97,8 +109,10 @@ class cfparse(_CommonParse):
     """cfparse step parser."""
 
     def __init__(self, name, *args, **kwargs):
-        """Compile parse expression."""
+        """Stringify"""
+        name = str(name, **({"encoding": "utf-8"} if isinstance(name, bytes) else {}))
         super().__init__(name)
+        """Compile parse expression."""
         self.parser = base_cfparse.Parser(self.name, *args, **kwargs)
 
 
@@ -122,7 +136,7 @@ class string(StepParser):
         return self.name == name
 
 
-def get_parser(step_name):
+def get_parser(step_name: Union[str, StepParser, StepParserProtocol]) -> Union[StepParser, StepParserProtocol]:
     """Get parser by given name.
 
     :param step_name: name of the step to parse
@@ -131,14 +145,11 @@ def get_parser(step_name):
     :rtype: StepArgumentParser
     """
 
-    def does_support_parser_interface(obj):
-        return all(map(partial(hasattr, obj), ["is_matching", "parse_arguments"]))
-
-    if does_support_parser_interface(step_name):
+    if isinstance(step_name, StepParserProtocol):
         return step_name
     elif isinstance(step_name, _Re_Pattern):
         return _re(step_name)
     elif isinstance(step_name, base_parse.Parser):
         return _parse(step_name)
     else:
-        return string(step_name)
+        return cfparse(step_name)
