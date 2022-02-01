elchupanebrej/pytest-bdd-ng

step functions could define multiple target fixtures

tests/library/test_parent.py
@@ -233,3 +233,109 @@ def test_local(testdir):
     )
     result = testdir.runpytest()
     result.assert_outcomes(passed=1)
+
+
+def test_local_multiple_target_fixtures(testdir):
+    """Test locally overridden fixtures."""
+    testdir.makeconftest(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given
+
+                @given("I have a parent fixtures", target_fixtures=["parent", "overridable"])
+                def parent():
+                    return "parent1", "parent2"
+                """
+        )
+    )
+
+    subdir = testdir.mkpydir("subdir")
+
+    subdir.join("local.feature").write(
+        textwrap.dedent(
+            """\
+                Feature: Local
+                    Scenario: Local override
+                        Given I have a parent fixture
+                """
+        ),
+    )
+
+    subdir.join("test_library.py").write(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given, scenario
+                from pytest_bdd.steps import get_step_fixture_name, GIVEN
+
+                @given("I have a parent fixture", target_fixtures=["parent", "overridable"])
+                def parent():
+                    return "local1", "local2"
+
+                @scenario("local.feature", "Local override")
+                def test_local(request):
+                    assert request.getfixturevalue("parent") == "local1"
+                    assert request.getfixturevalue("overridable") == "local2"
+
+
+                    fixture = request.getfixturevalue(
+                        get_step_fixture_name("I have a parent fixture", GIVEN)
+                    )
+                    assert fixture() == ("local1", "local2")
+                """
+        )
+    )
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_local_both_target_fixture_and_target_fixtures(testdir):
+    """Test locally overridden fixtures."""
+    testdir.makeconftest(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given
+
+                @given("I have a parent fixtures", target_fixture="parent", target_fixtures=["overridable"])
+                def parent():
+                    return "parent1", "parent2"
+                """
+        )
+    )
+
+    subdir = testdir.mkpydir("subdir")
+
+    subdir.join("local.feature").write(
+        textwrap.dedent(
+            """\
+                Feature: Local
+                    Scenario: Local override
+                        Given I have a parent fixture
+                """
+        ),
+    )
+
+    subdir.join("test_library.py").write(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given, scenario
+                from pytest_bdd.steps import get_step_fixture_name, GIVEN
+
+                @given("I have a parent fixture", target_fixtures=["parent", "overridable"])
+                def parent():
+                    return "local1", "local2"
+
+                @scenario("local.feature", "Local override")
+                def test_local(request):
+                    assert request.getfixturevalue("parent") == "local1"
+                    assert request.getfixturevalue("overridable") == "local2"
+
+
+                    fixture = request.getfixturevalue(
+                        get_step_fixture_name("I have a parent fixture", GIVEN)
+                    )
+                    assert fixture() == ("local1", "local2")
+                """
+        )
+    )
+    result = testdir.runpytest("-W", "ignore::pytest_bdd.PytestBDDStepDefinitionWarning")
+    result.assert_outcomes(passed=1)

pytest-dev/pytest-bdd

tests/library/test_parent.py
@@ -233,3 +233,109 @@ def test_local(testdir):
     )
     result = testdir.runpytest()
     result.assert_outcomes(passed=1)
+
+
+def test_local_multiple_target_fixtures(testdir):
+    """Test locally overridden fixtures."""
+    testdir.makeconftest(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given
+
+                @given("I have a parent fixtures", target_fixtures=["parent", "overridable"])
+                def parent():
+                    return "parent1", "parent2"
+                """
+        )
+    )
+
+    subdir = testdir.mkpydir("subdir")
+
+    subdir.join("local.feature").write(
+        textwrap.dedent(
+            """\
+                Feature: Local
+                    Scenario: Local override
+                        Given I have a parent fixture
+                """
+        ),
+    )
+
+    subdir.join("test_library.py").write(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given, scenario
+                from pytest_bdd.steps import get_step_fixture_name, GIVEN
+
+                @given("I have a parent fixture", target_fixtures=["parent", "overridable"])
+                def parent():
+                    return "local1", "local2"
+
+                @scenario("local.feature", "Local override")
+                def test_local(request):
+                    assert request.getfixturevalue("parent") == "local1"
+                    assert request.getfixturevalue("overridable") == "local2"
+
+
+                    fixture = request.getfixturevalue(
+                        get_step_fixture_name("I have a parent fixture", GIVEN)
+                    )
+                    assert fixture() == ("local1", "local2")
+                """
+        )
+    )
+    result = testdir.runpytest()
+    result.assert_outcomes(passed=1)
+
+
+def test_local_both_target_fixture_and_target_fixtures(testdir):
+    """Test locally overridden fixtures."""
+    testdir.makeconftest(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given
+
+                @given("I have a parent fixtures", target_fixture="parent", target_fixtures=["overridable"])
+                def parent():
+                    return "parent1", "parent2"
+                """
+        )
+    )
+
+    subdir = testdir.mkpydir("subdir")
+
+    subdir.join("local.feature").write(
+        textwrap.dedent(
+            """\
+                Feature: Local
+                    Scenario: Local override
+                        Given I have a parent fixture
+                """
+        ),
+    )
+
+    subdir.join("test_library.py").write(
+        textwrap.dedent(
+            """\
+                from pytest_bdd import given, scenario
+                from pytest_bdd.steps import get_step_fixture_name, GIVEN
+
+                @given("I have a parent fixture", target_fixtures=["parent", "overridable"])
+                def parent():
+                    return "local1", "local2"
+
+                @scenario("local.feature", "Local override")
+                def test_local(request):
+                    assert request.getfixturevalue("parent") == "local1"
+                    assert request.getfixturevalue("overridable") == "local2"
+
+
+                    fixture = request.getfixturevalue(
+                        get_step_fixture_name("I have a parent fixture", GIVEN)
+                    )
+                    assert fixture() == ("local1", "local2")
+                """
+        )
+    )
+    result = testdir.runpytest_subprocess("-W", "ignore::pytest_bdd.PytestBDDStepDefinitionWarning")
+    result.assert_outcomes(passed=1)