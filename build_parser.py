a77bde1574916938a1539f57e4f1a2cff1fdd535
Alessio Bogon <youtux@gmail.com>
2022-04-15 23:53:19 +0200

    Fix tox install

--- build_parser.py
+++ build_parser.py
@@ -1,10 +1,22 @@
 import pathlib
 
 import tatsu
+from setuptools.command.build_py import build_py as original_build_py
 from setuptools.command.sdist import sdist as original_sdist
 
 
+# TODO: Figure out if we need sdist or build_py
 class sdist(original_sdist):
+    def run(self) -> None:
+        # Build the generated parser (_gherkin.py)
+        grammar = pathlib.Path("pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
+        compiled_py = tatsu.to_python_sourcecode(grammar)
+        pathlib.Path("pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
+
+        return super().run()
+
+
+class build_py(original_build_py):
     def run(self) -> None:
         # Build the generated parser (_gherkin.py)
         grammar = pathlib.Path("pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
