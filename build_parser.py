07f1702a3b3ee250018b3200bcb7dbe47b75e19c
Alessio Bogon <youtux@gmail.com>
2022-04-15 22:57:58 +0200

    Fix builder, add it to the sdist

--- build_parser.py
+++ build_parser.py
@@ -7,7 +7,7 @@ from setuptools.command.sdist import sdist as original_sdist
 class sdist(original_sdist):
     def run(self) -> None:
         # Build the generated parser (_gherkin.py)
-        grammar = pathlib.Path("pytest_bdd/parser_data/gherkin.tatsu").read_text(encoding="utf-8")
+        grammar = pathlib.Path("pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
         compiled_py = tatsu.to_python_sourcecode(grammar)
         pathlib.Path("pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
 
