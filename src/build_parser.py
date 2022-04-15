24ac9252407eaba226d90e25288a243039877780
Alessio Bogon <youtux@gmail.com>
2022-04-16 00:52:52 +0200

    use "src/" layout (https://setuptools.pypa.io/en/latest/userguide/package_discovery.html#src-layout).
    The non-src layout was causing problem when running pytest with tox on CI, since it would try to import from the cwd, rather than from the installed packages

--- src/build_parser.py
+++ src/build_parser.py
@@ -9,9 +9,9 @@ from setuptools.command.sdist import sdist as original_sdist
 class sdist(original_sdist):
     def run(self) -> None:
         # Build the generated parser (_gherkin.py)
-        grammar = pathlib.Path("pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
+        grammar = pathlib.Path("src/pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
         compiled_py = tatsu.to_python_sourcecode(grammar)
-        pathlib.Path("pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
+        pathlib.Path("src/pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
 
         return super().run()
 
@@ -19,8 +19,8 @@ class sdist(original_sdist):
 class build_py(original_build_py):
     def run(self) -> None:
         # Build the generated parser (_gherkin.py)
-        grammar = pathlib.Path("pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
+        grammar = pathlib.Path("src/pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
         compiled_py = tatsu.to_python_sourcecode(grammar)
-        pathlib.Path("pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
+        pathlib.Path("src/pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
 
         return super().run()
