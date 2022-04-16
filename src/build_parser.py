a442615b691442facd6f28a40fbf5ac13e48905b
Alessio Bogon <youtux@gmail.com>
2022-04-16 12:15:55 +0200

    we only need build_py

--- src/build_parser.py
+++ src/build_parser.py
@@ -1,22 +1,10 @@
 import pathlib
 
 import tatsu
-from setuptools.command.build_py import build_py as original_build_py
-from setuptools.command.sdist import sdist as original_sdist
+from setuptools.command.build_py import build_py as _build_py
 
 
-# TODO: Figure out if we need sdist or build_py
-class sdist(original_sdist):
-    def run(self) -> None:
-        # Build the generated parser (_gherkin.py)
-        grammar = pathlib.Path("src/pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
-        compiled_py = tatsu.to_python_sourcecode(grammar)
-        pathlib.Path("src/pytest_bdd/_gherkin.py").write_text(compiled_py, encoding="utf-8")
-
-        return super().run()
-
-
-class build_py(original_build_py):
+class build_py(_build_py):
     def run(self) -> None:
         # Build the generated parser (_gherkin.py)
         grammar = pathlib.Path("src/pytest_bdd/gherkin.tatsu").read_text(encoding="utf-8")
