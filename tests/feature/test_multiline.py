4fc500161dd7da5fd5c1d745ef46f281c9e528e0
Kostiantyn Goloveshko <KGoloveshko@luxoft.com>
2022-04-28 10:48:52 +0300

    Re-enable tests for original parser

--- tests/feature/test_multiline.py
+++ tests/feature/test_multiline.py
@@ -1,62 +1,53 @@
 """Multiline steps tests."""
 import textwrap
 
-import pytest
 from pytest import mark, param
 
 
-@mark.parametrize("parser,", [param("Parser", marks=[mark.deprecated, mark.deficient, mark.skip]), "GherkinParser"])
-@pytest.mark.parametrize(
+@mark.parametrize(
+    "parser,",
+    [
+        param("Parser", marks=[mark.deprecated, mark.surplus]),
+    ],
+)
+@mark.parametrize(
     ["feature_text", "expected_text"],
     [
         (
-            textwrap.dedent(
-                '''\
+            """\
             Feature: Multiline
                 Scenario: Multiline step using sub indentation
                     Given I have a step with:
-                        """
                         Some
 
                         Extra
                         Lines
-                        """
                     Then the text should be parsed with correct indentation
-            '''
-            ),
+            """,
             "Some\n\nExtra\nLines",
         ),
         (
-            textwrap.dedent(
-                '''\
+            """\
             Feature: Multiline
                 Scenario: Multiline step using sub indentation
                     Given I have a step with:
-                     """
                         Some
 
                       Extra
                      Lines
-                     """
                     Then the text should be parsed with correct indentation
-            '''
-            ),
+            """,
             "   Some\n\n Extra\nLines",
         ),
         (
-            textwrap.dedent(
-                '''\
+            """\
             Feature: Multiline
                 Scenario: Multiline step using sub indentation
                     Given I have a step with:
-                        """
                         Some
                         Extra
                         Lines
-                        """
-
-            '''
-            ),
+            """,
             "Some\nExtra\nLines",
         ),
     ],
@@ -65,32 +56,82 @@ def test_multiline(testdir, feature_text, expected_text, parser):
     testdir.makefile(".feature", multiline=feature_text)
 
     testdir.makepyfile(
-        textwrap.dedent(
-            f"""\
-            from pytest_bdd import parsers, given, then, scenario
-            from pytest_bdd.parser import {parser} as Parser
+        """\
+        from pytest_bdd import parsers, given, then, scenario
+        from pytest_bdd.parser import {parser} as Parser
 
-            expected_text = '''{{expected_text}}'''
-
-
-            @scenario("multiline.feature", "Multiline step using sub indentation", _parser=Parser())
-            def test_multiline(request):
-                pass
+        expected_text = '''{expected_text}'''
 
 
-            @given(parsers.parse("I have a step with:"), target_fixture="text")
-            def i_have_text(step):
-                return step.doc_string.content
+        @scenario("multiline.feature", "Multiline step using sub indentation", _parser=Parser())
+        def test_multiline(request):
+            pass
 
 
-            @then("the text should be parsed with correct indentation")
-            def text_should_be_correct(text):
-                assert text == expected_text
+        @given(parsers.parse("I have a step with:\\n{{text}}"), target_fixture="text")
+        def i_have_text(text):
+            return text
 
-            """.format(
-                expected_text=expected_text.encode("unicode_escape").decode("utf-8"),
-            )
+
+        @then("the text should be parsed with correct indentation")
+        def text_should_be_correct(text):
+            assert text == expected_text
+        """.format(
+            expected_text=expected_text.encode("unicode_escape").decode("utf-8"),
+            parser=parser,
         )
     )
     result = testdir.runpytest()
     result.assert_outcomes(passed=1)
+
+
+@mark.parametrize(
+    "parser,",
+    [
+        param("Parser", marks=[mark.deprecated, mark.surplus]),
+    ],
+)
+def test_multiline_wrong_indent(testdir, parser):
+    """Multiline step using sub indentation wrong indent."""
+
+    testdir.makefile(
+        ".feature",
+        multiline=textwrap.dedent(
+            """\
+
+            Feature: Multiline
+                Scenario: Multiline step using sub indentation wrong indent
+                    Given I have a step with:
+                        Some
+
+                    Extra
+                    Lines
+                    Then the text should be parsed with correct indentation
+            """
+        ),
+    )
+
+    testdir.makepyfile(
+        f"""\
+        from pytest_bdd import parsers, given, then, scenario
+        from pytest_bdd.parser import {parser} as Parser
+
+
+        @scenario("multiline.feature", "Multiline step using sub indentation wrong indent", _parser=Parser())
+        def test_multiline(request):
+            pass
+
+
+        @given(parsers.parse("I have a step with:\\n{{text}}"))
+        def i_have_text(text):
+            return text
+
+
+        @then("the text should be parsed with correct indentation")
+        def text_should_be_correct(i_have_text, text):
+            assert i_have_text == text == expected_text
+        """
+    )
+    result = testdir.runpytest()
+    result.assert_outcomes(failed=1)
+    result.stdout.fnmatch_lines("*StepDefinitionNotFoundError: Step definition is not found:*")
