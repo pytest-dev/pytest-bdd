6f95c5cacdafc954b8d60d6fcf93227230f2623a
Alessio Bogon <778703+youtux@users.noreply.github.com>
2022-07-08 21:55:14 +0200

    Remove version from __init__.py, as we track it in pyproject.toml

--- pytest_bdd/__init__.py
+++ pytest_bdd/__init__.py
@@ -4,6 +4,4 @@ from __future__ import annotations
 from pytest_bdd.scenario import scenario, scenarios
 from pytest_bdd.steps import given, then, when
 
-__version__ = "6.0.1"
-
 __all__ = ["given", "when", "then", "scenario", "scenarios"]
