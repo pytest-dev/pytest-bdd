61c48285ee3ac1ad90d9589aa125b1a983977256
Kostiantyn Goloveshko <kostyagoloveshko@gmail.com>
2022-10-17 23:27:00 +0300

    Remove migrate scripts for old pytest-bdd versions

--- pytest_bdd/scripts.py
+++ pytest_bdd/scripts.py
@@ -3,38 +3,8 @@ from __future__ import annotations
 
 import argparse
 import os.path
-import re
-from pathlib import Path
 
 from pytest_bdd.generation import generate_code, parse_feature_files
-
-MIGRATE_REGEX = re.compile(r"\s?(\w+)\s=\sscenario\((.+)\)", flags=re.MULTILINE)
-
-
-def migrate_tests(args):
-    """Migrate outdated tests to the most recent form."""
-    path = args.path
-    for file_path in Path(path).rglob("*.py"):
-        migrate_tests_in_file(file_path)
-
-
-def migrate_tests_in_file(file_path):
-    """Migrate all bdd-based tests in the given test file."""
-    try:
-        with file_path.open("r+") as fd:
-            content = fd.read()
-            new_content = MIGRATE_REGEX.sub(r"\n@scenario(\2)\ndef \1():\n    pass\n", content)
-            if new_content != content:
-                # the regex above potentially causes the end of the file to
-                # have an extra newline
-                new_content = new_content.rstrip("\n") + "\n"
-                fd.seek(0)
-                fd.write(new_content)
-                print(f"migrated: {file_path}")
-            else:
-                print(f"skipped: {file_path}")
-    except OSError:
-        pass
 
 
 def check_existense(file_name):
@@ -65,10 +35,6 @@ def main():
         help="Feature files to generate test code with",
     )
     parser_generate.set_defaults(func=print_generated_code)
-
-    parser_migrate = subparsers.add_parser("migrate", help="migrate help")
-    parser_migrate.add_argument("path", metavar="PATH", help="Migrate outdated tests to the most recent form")
-    parser_migrate.set_defaults(func=migrate_tests)
 
     args = parser.parse_args()
     if hasattr(args, "func"):
