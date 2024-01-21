32a19ce09f7d2be160c733e337885b81e806b0cf
Alessio Bogon <778703+youtux@users.noreply.github.com>
2024-01-21 15:41:48 +0100

    Use annotations compatible with py3.8

--- src/pytest_bdd/compat.py
+++ src/pytest_bdd/compat.py
@@ -1,6 +1,7 @@
+from __future__ import annotations
+
 from collections.abc import Sequence
 from importlib.metadata import version
-from typing import Optional
 
 from _pytest.fixtures import FixtureDef, FixtureManager
 from _pytest.nodes import Node
@@ -12,10 +13,10 @@ pytest_version = parse_version(version("pytest"))
 
 if pytest_version >= Version("8.1"):
 
-    def getfixturedefs(fixturemanager: FixtureManager, fixturename: str, node: Node) -> Optional[Sequence[FixtureDef]]:
+    def getfixturedefs(fixturemanager: FixtureManager, fixturename: str, node: Node) -> Sequence[FixtureDef] | None:
         return fixturemanager.getfixturedefs(fixturename, node)
 
 else:
 
-    def getfixturedefs(fixturemanager: FixtureManager, fixturename: str, node: Node) -> Optional[Sequence[FixtureDef]]:
+    def getfixturedefs(fixturemanager: FixtureManager, fixturename: str, node: Node) -> Sequence[FixtureDef] | None:
         return fixturemanager.getfixturedefs(fixturename, node.nodeid)
