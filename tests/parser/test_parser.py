c3008c1debe61dd2cc9d6dc799db7048b1467c25
Jason Allen <jsa34@cam.ac.uk>
2024-09-12 21:39:15 +0100

    Couple of tidy ups

--- tests/parser/test_parser.py
+++ tests/parser/test_parser.py
@@ -4,13 +4,8 @@ from src.pytest_bdd.gherkin_parser import get_gherkin_document
 
 
 def test_parser():
-    # Get the directory of the current file
     test_dir = Path(__file__).parent
-
-    # Resolve the path to the feature file relative to the test directory
     feature_file = test_dir / "test.feature"
-
-    # Convert to string if necessary, but Path objects are often used directly
     feature_file_path = str(feature_file.resolve())
 
     get_gherkin_document(feature_file_path)
