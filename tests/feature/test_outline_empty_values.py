e181ced0b422a5d47326f9b3b7ab400accc86306
Jason Allen <jsa34@cam.ac.uk>
2024-12-01 21:03:13 +0000

    Remove unnecessary test

--- tests/feature/test_outline_empty_values.py
+++ tests/feature/test_outline_empty_values.py
@@ -4,6 +4,28 @@ import textwrap
 
 from pytest_bdd.utils import collect_dumped_objects
 
+STEPS = """\
+from pytest_bdd import given, when, then, parsers
+from pytest_bdd.utils import dump_obj
+
+# Using `parsers.re` so that we can match empty values
+
+@given(parsers.re("there are (?P<start>.*?) cucumbers"))
+def _(start):
+    dump_obj(start)
+
+
+@when(parsers.re("I eat (?P<eat>.*?) cucumbers"))
+def _(eat):
+    dump_obj(eat)
+
+
+@then(parsers.re("I should have (?P<left>.*?) cucumbers"))
+def _(left):
+    dump_obj(left)
+
+"""
+
 
 def test_scenario_with_empty_example_values(pytester):
     pytester.makefile(
@@ -22,36 +44,14 @@ def test_scenario_with_empty_example_values(pytester):
             """
         ),
     )
-    pytester.makeconftest(
-        textwrap.dedent(
-            """\
-            from pytest_bdd import given, when, then, parsers
-            from pytest_bdd.utils import dump_obj
-
-            # Using `parsers.re` so that we can match empty values
-
-            @given(parsers.re("there are (?P<start>.*?) cucumbers"))
-            def _(start):
-                dump_obj(start)
-
-
-            @when(parsers.re("I eat (?P<eat>.*?) cucumbers"))
-            def _(eat):
-                dump_obj(eat)
-
-
-            @then(parsers.re("I should have (?P<left>.*?) cucumbers"))
-            def _(left):
-                dump_obj(left)
-
-            """
-        )
-    )
+    pytester.makeconftest(textwrap.dedent(STEPS))
 
     pytester.makepyfile(
         textwrap.dedent(
             """\
+        from pytest_bdd.utils import dump_obj
         from pytest_bdd import scenario
+        import json
 
         @scenario("outline.feature", "Outlined with empty example values")
         def test_outline():
@@ -62,69 +62,3 @@ def test_scenario_with_empty_example_values(pytester):
     result = pytester.runpytest("-s")
     result.assert_outcomes(passed=1)
     assert collect_dumped_objects(result) == ["#", "", ""]
-
-
-def test_scenario_with_empty_example_values_none_transformer(pytester):
-    """
-    Checks that `parsers.re` can transform empty values to None with a converter.
-    `parsers.parse` and `parsers.cfparse` won't work out of the box this way as they will fail to match the steps.
-    """
-    pytester.makefile(
-        ".feature",
-        outline=textwrap.dedent(
-            """\
-            Feature: Outline
-                Scenario Outline: Outlined with empty example values and transformer
-                    Given there are <start> cucumbers
-                    When I eat <eat> cucumbers
-                    Then I should have <left> cucumbers
-
-                    Examples:
-                    | start | eat | left |
-                    | #     |     |      |
-            """
-        ),
-    )
-    pytester.makeconftest(
-        textwrap.dedent(
-            """\
-            from pytest_bdd import given, when, then, parsers
-            from pytest_bdd.utils import dump_obj
-
-
-            def empty_to_none(value):
-                return None if value.strip() == "" else value
-
-
-            @given(parsers.re("there are (?P<start>.*?) cucumbers"), converters={"start": empty_to_none})
-            def _(start):
-                dump_obj(start)
-
-
-            @when(parsers.re("I eat (?P<eat>.*?) cucumbers"), converters={"eat": empty_to_none})
-            def _(eat):
-                dump_obj(eat)
-
-
-            @then(parsers.re("I should have (?P<left>.*?) cucumbers"), converters={"left": empty_to_none})
-            def _(left):
-                dump_obj(left)
-
-            """
-        )
-    )
-
-    pytester.makepyfile(
-        textwrap.dedent(
-            """\
-            from pytest_bdd import scenario
-
-            @scenario("outline.feature", "Outlined with empty example values and transformer")
-            def test_outline():
-                pass
-            """
-        )
-    )
-    result = pytester.runpytest("-s")
-    result.assert_outcomes(passed=1)
-    assert collect_dumped_objects(result) == ["#", None, None]
